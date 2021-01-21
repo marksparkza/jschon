366d6d5f58c356b966ec26b6fae90b7a5c59e6e2
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-01-21 08:41:23 +0200

    Update URI test to handle percent-escapes

--- tests/test_uri.py
+++ tests/test_uri.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 from hypothesis import given, provisional as hp
 
 from jschon.uri import URI
@@ -6,5 +8,5 @@ from jschon.uri import URI
 @given(hp.urls())
 def test_create_uri(value):
     uri = URI(value)
-    assert str(uri) == value
+    assert urllib.parse.unquote(str(uri)) == urllib.parse.unquote(value)
     assert eval(repr(uri)) == uri
