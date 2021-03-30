0ecc0686f5f298b0d62877ac54260dc01be365a4
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-03-31 00:59:23 +0200

    Prepare package for distribution :gift:

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change log
+
+## v0.1.0 (2021-03-31)
+
+### Features
+
+* JSON data model
+* JSON Pointer implementation
+* JSON Schema implementation, supporting drafts 2019-09 and 2020-12 of the specification
+* URI class (wraps rfc3986.URIReference)
--- setup.py
+++ setup.py
@@ -1,25 +1,36 @@
+import pathlib
+
 from setuptools import setup, find_packages
 
+HERE = pathlib.Path(__file__).parent.resolve()
+README = (HERE / 'README.md').read_text(encoding='utf-8')
+
 setup(
-    name='Jschon',
+    name='jschon',
     version='0.1.0',
-    description='A pythonic, extensible JSON Schema implementation',
+    description='A pythonic, extensible JSON Schema implementation.',
+    long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/marksparkza/jschon',
     author='Mark Jacobson',
-    python_requires='~=3.8',
-    packages=find_packages(),
-    install_requires=[
-        'rfc3986',
-    ],
-    extras_require={
-        'test': ['tox'],
-    },
+    author_email='mark@saeon.ac.za',
+    license='MIT',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    python_requires='~=3.8',
+    install_requires=['rfc3986'],
+    extras_require={'test': ['tox']},
 )
