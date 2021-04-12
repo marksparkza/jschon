5ecab878161fa06ff0bc66c9d1a78c5bfd03f5aa
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-04-12 14:22:16 +0200

    Bump version for publish workflow test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='jschon',
-    version='0.1.1',
+    version='0.1.2',
     description='A pythonic, extensible JSON Schema implementation.',
     long_description=README,
     long_description_content_type='text/markdown',
