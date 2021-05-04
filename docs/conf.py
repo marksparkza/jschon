f5c012daff7419d7808030c67b9bd6904d02e6b3
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-05-04 08:49:50 +0200

    Remove empty directories

--- docs/conf.py
+++ docs/conf.py
@@ -4,10 +4,8 @@ copyright = '2021, Mark Jacobson'
 release = '0.2.0'
 
 extensions = ['sphinx.ext.autodoc']
-templates_path = ['_templates']
 exclude_patterns = ['_build']
 html_theme = 'classic'
-html_static_path = ['_static']
 
 autodoc_default_options = {
     'members': True,
